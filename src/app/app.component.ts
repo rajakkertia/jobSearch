df71ff26c4b5172281a361137a9471bffc7e166f
rajakkertia <sandro.baramidze@gmail.com>
2025-07-15 18:52:25 +0400

    bookmark page with navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule, RouterModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'job-search';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { JobListComponent } from './job-search/job-list.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: JobListComponent,
-  }
+  { path: '', component: JobListComponent },
+  { path: 'bookmarks', component: BookmarksComponent }
 ];
