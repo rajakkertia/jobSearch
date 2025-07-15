0e4d0446349bdcdde4d54d9b554877b17df85da7
rajakkertia <sandro.baramidze@gmail.com>
2025-07-15 17:15:30 +0400

    skeleton loading added

--- src/app/job-search/job-list.component.ts
+++ src/app/job-search/job-list.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { JobListService } from './job-list.service';
+import { SkeletonCardComponent } from '../shared/components/skeleton/skeleton-card.component';
 
 @Component({
   selector: 'app-job-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, SkeletonCardComponent],
   templateUrl: './job-list.component.html',
   styleUrl: './job-list.component.scss'
 })
@@ -14,6 +15,7 @@ export class JobListComponent implements OnInit {
 
   private jobsService = inject(JobListService);
   jobs = signal<any[]>([]);
+  loading = signal(false);
   query = 'Angular Developer';
   locations = [
     'Worldwide',
@@ -31,7 +33,7 @@ export class JobListComponent implements OnInit {
   selectedLocation = 'Worldwide';
 
   constructor() {
-    this.searchJobs();
+    // this.searchJobs();
   }
 
   ngOnInit() {
@@ -40,9 +42,15 @@ export class JobListComponent implements OnInit {
   }
 
   searchJobs() {
+    this.loading.set(true);
+    this.jobs.set([]); // Clear previous jobs
     const location = this.selectedLocation === 'Worldwide' ? '' : this.selectedLocation;
     this.jobsService.searchJobs(this.query, location).subscribe((res) => {
       this.jobs.set(res.data || []);
+      this.loading.set(false);
+    }, (error) => {
+      console.error('Error fetching jobs:', error);
+      this.loading.set(false);
     });
   }
 
